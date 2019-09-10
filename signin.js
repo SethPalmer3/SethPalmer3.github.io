88918f20d31adf9b1b65c6ad24f794da4f5365ba
TeacherSignIn <54999645+TeacherSignIn@users.noreply.github.com>
2019-09-10 10:40:34 -0700

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,6 +1,6 @@
 var long;
 var lat;
-var form = document.getElementsByTagName("iframe")[0];
+var form = document.getElementsByClassName("frame")[0];
 form.style.display = "none";
 
 function showPosition(position){
