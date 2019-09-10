ada0b4f1f226eb8324ae66f738f6cf6d400e9d7f
TeacherSignIn <54999645+TeacherSignIn@users.noreply.github.com>
2019-09-10 10:13:51 -0700

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,19 +1,6 @@
 function showPosition(position){
   console.log(position.coords.longitude);
 }
-
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
-
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
-    }
-}
 function send(){
   var win = window.open('https://script.google.com/macros/s/AKfycbwAi2yZEWJQeGJMYXlP729X9Xop4CDSeG-V-cn5vePh_GWjeNmC/exec');
   win.close();
