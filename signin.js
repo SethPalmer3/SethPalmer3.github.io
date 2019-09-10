3d21bccf62ca01b40022c8bf87f74c01e6cd269c
TeacherSignIn <54999645+TeacherSignIn@users.noreply.github.com>
2019-09-10 11:09:21 -0700

    Update signin.js

--- signin.js
+++ signin.js
@@ -11,7 +11,7 @@ function showPosition(position){
 }
 
 function showForm(){
-  if (long=-112.6925169){
+  if (long = -112.6925169){
     console.log('work');
     form.style.display = "block";
   }
